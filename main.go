a05919335b18b784648e8e7cd76d04d54258e28f
Mohamed Alsoudani <mr.hassuny@gmail.com>
2019-12-04 23:22:03 -0700

    can view the list of processes and autoupdate every 3 seconds

--- main.go
+++ main.go
@@ -5,24 +5,24 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 func main() {
 	cmdArr := strings.Fields("ps")
-	// clear := func() {
-	// 	cmd := exec.Command("clear")
-	// 	cmd.Stdout = os.Stdout
-	// 	cmd.Run()
-	// }
+	clear := func() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
 	for {
 		cmd := exec.Command(cmdArr[0], cmdArr[1:]...)
-		if cmd.Stdout != os.Stdout {
-			cmd.Stdout = os.Stdout
-			if err := cmd.Run(); err != nil {
-				fmt.Println(os.Stderr, err)
-			}
+		cmd.Stdout = os.Stdout
+		if err := cmd.Run(); err != nil {
+			fmt.Println(os.Stderr, err)
 		}
 		cmd.Stdout = os.Stdout
-		// clear()
+		time.Sleep(3 * time.Second)
+		clear()
 	}
 }
